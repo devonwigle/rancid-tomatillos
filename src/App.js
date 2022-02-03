74836b951335fb6e760715790d08b23419cd9833
Devon Wigle <devonwigle@gmail.com>
2022-02-03 17:09:02 -0600

    Build app class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 
 import React, {Component} from 'react'
+import movieData from './data.js'
+import MovieContainer from './Components/MovieContainer'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  
+  render() {
+    return ()
+  }
+}
 
 export default App;
