5a195bed126f0994a8d973dcbad3062539173450
Devon Wigle <devonwigle@gmail.com>
2022-02-03 17:53:20 -0600

    Add classname to movie poster image

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -4,7 +4,7 @@ import '../CSS/MovieCard.css'
 const MovieCard = ({poster_path, backdrop_path, title, average_rating, release_date}) => {
   return (
     <div className="card">
-      <img src={poster_path} />
+      <img className="card-image" src={poster_path} />
       <p>Title: {title}</p>
       <p>User Rating: {average_rating}</p>
     </div>
