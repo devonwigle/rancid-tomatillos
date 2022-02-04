fd6b2599e2db5ab5717787b2012a669904555f6b
Emma Chory <emmachory@gmail.com>
2022-02-04 11:10:09 -0700

    Add event listener on click

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import './App.css'
 class App extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      selectedMovie: false
+    }
   }
   
   render() {
--- src/Components/MovieContainer.js
+++ src/Components/MovieContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import MovieCard from './MovieCard'
 import '../CSS/MovieContainer.css'
 
-const MovieContainer = ({data}) => {
+const MovieContainer = ({data, showDetails}) => {
   console.log({data})
   const movieCardComponents = data.movies.map(movie => {
     return (
@@ -10,6 +10,7 @@ const MovieContainer = ({data}) => {
         poster_path={movie.poster_path}
         title={movie.title}
         average_rating={movie.average_rating.toFixed(2)}
+        id={movie.id}
         key={movie.id}
       />
     )
