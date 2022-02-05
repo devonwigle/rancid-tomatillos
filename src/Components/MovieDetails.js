21aafafe3e472c74c6c3abe838c20a3934f4ce3e
Emma Chory <emmachory@gmail.com>
2022-02-04 19:05:33 -0700

    Fix image sizing

--- src/Components/MovieDetails.js
+++ src/Components/MovieDetails.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../CSS/MovieDetails.css'
 
 const MovieDetails = ({id, backdrop_path, title, release_date, overview, average_rating}) => {
   return(
     <div>
-      <img src={backdrop_path} />
+      <img className="backdrop" src={backdrop_path} />
       <p>{title}</p>
       <p>User Rating: {average_rating}</p>
       <p>Release Date: {release_date}</p>
