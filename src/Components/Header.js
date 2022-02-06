ad62b923ec6943f45a9b52a1a510355f917e5303
Emma Chory <emmachory@gmail.com>
2022-02-05 17:13:28 -0700

    Change individual movie styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import App from '../App';
 import '../CSS/Header.css';
-
+import tomato from '../assets/tomato.png'
 
 const Header = ({goHome}) => {
 return(
   <div className="header">
-  <h1 className="title">Rancid Tomatillos</h1>
+  <h1 className="title">Rancid Tomatillos <img className="tomato" src={tomato}/></h1>
   <button onClick={() => goHome()} className="home-button">Home</button>
   </div>
 )
