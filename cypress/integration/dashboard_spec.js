ff468f27dab6f4435146ce5a145ca467c15398b9
Devon Wigle <devonwigle@gmail.com>
2022-02-07 20:52:05 -0600

    Add testing for seeing all movies and movie details

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -3,8 +3,28 @@ describe('Movie flows', () => {
     expect(true).to.equal(true)
   })
 
-  it('Should be able to visit the page', () => {
+  it('Should be able to visit the page and see all movies', () => {
     cy.visit('http://localhost:3000')
       .contains('Rancid Tomatillos')
+      .get('.card')
+      .should('have.length', 40)
+  })
+
+  it('Should be able to see movie details', () => {
+    cy.visit('http://localhost:3000') 
+      .get('.card').first().click()
+      .get('.movie-details')
+      .get('.details')
+      .contains('Money Plane')
+  })
+
+  it('Should be able to go back home', () => {
+    cy.visit('http://localhost:3000')
+      .get('.card').first().click()
+      .get('.home-button').click()
+      .should()
+      .get('.movie-container')
+      .get('.card')
+      .should('have.length', 40)
   })
 })
