68ab78a7257ee4038dc3fdae613d251d191fbeca
Devon Wigle <devonwigle@gmail.com>
2022-02-13 17:45:51 -0600

    Fix gohome function

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -4,13 +4,43 @@ describe('Movie flows', () => {
   })
 
   it('Should be able to visit the page and see all movies', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/',
+      {
+        statusCode: 200,
+        ok: true,
+        body: {
+          movies: [{
+            'id': 694919,
+            'poster_path': 'https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg',
+            'backdrop_path': 'https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg',
+            'title': 'Money Plane',
+            'average_rating': 6.666666666666667,
+            'release_date': '2020-09-29'
+          }]
+        }
+      })
     cy.visit('http://localhost:3000')
       .contains('Rancid Tomatillos')
       .get('.card')
-      .should('have.length', 40)
+      .should('have.length', 1)
   })
 
   it('Should be able to see movie details', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/',
+      {
+        statusCode: 200,
+        ok: true,
+        body: {
+          movies: [{
+            'id': 694919,
+            'poster_path': 'https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg',
+            'backdrop_path': 'https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg',
+            'title': 'Money Plane',
+            'average_rating': 6.666666666666667,
+            'release_date': '2020-09-29'
+          }]
+        }
+      })
     cy.visit('http://localhost:3000') 
       .get('.card').first().click()
       .get('.movie-details')
@@ -19,10 +49,23 @@ describe('Movie flows', () => {
   })
 
   it('Should be able to go back home', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/',
+      {
+        statusCode: 200,
+        ok: true,
+        body: {
+          movies: [{
+            'id': 694919,
+            'poster_path': 'https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg',
+            'backdrop_path': 'https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg',
+            'title': 'Money Plane',
+            'average_rating': 6.666666666666667,
+            'release_date': '2020-09-29'
+          }]
+        }
+      })
     cy.visit('http://localhost:3000')
       .get('.card').first().click()
       .get('.home-button').click()
-      // .get('.card')
-      // .should('have.length', 40)
   })
 })
