513a857a62dbd34c40ef544833c5577b9bfedb13
Devon Wigle <devonwigle@gmail.com>
2022-02-14 14:11:18 -0600

    Remove extra tomato

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,13 +6,11 @@ import tomato from '../assets/tomato.png'
 const Header = ({goHome}) => {
 return(
     <div className="header">
-      <img className="tomato" src={tomato} />
       <h1 className="title">Rancid Tomatillos</h1>
       <img className="tomato" src={tomato} />
       <Link to="/">
       <button onClick={goHome} className="home-button">Home</button>
       </Link>
-
     </div>
 )
 }
