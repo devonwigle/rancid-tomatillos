d1e9f26eb2271cfa9daaff429a023d1826c5e0fe
Emma Chory <emmachory@gmail.com>
2022-02-14 20:13:54 -0700

    Add tomato

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ import tomato from '../assets/tomato.png'
 const Header = () => {
 return(
     <div className="header">
-      <h1 className="title">Rancid Tomatillos <img className="tomato" src={tomato} /></h1>
+      <h1 className="title"><img className="tomato" src={tomato} /> Rancid Tomatillos <img className="tomato" src={tomato} /></h1>
     </div>
 )
 }
